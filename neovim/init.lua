b2fdd3671a14aa7c2a31e2429dcf49cd01d04862
PauloRSF <paulorsouzaf@gmail.com>
2022-10-07 18:41:05 -0300

    Add rust.vim, nvim-tree, gitsigns and nvim-comment to neovim plugins

--- neovim/init.lua
+++ neovim/init.lua
@@ -9,7 +9,6 @@ vim.opt.colorcolumn = { 80, 100, 120 }
 vim.opt.hidden = true
 vim.opt.cmdheight = 2
 vim.opt.updatetime = 300
-vim.opt.signcolumn = 'number'
 
 -- Indentation
 
@@ -34,4 +33,14 @@ vim.api.nvim_set_keymap('n', '<C-k>', ':BufferNext<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-w>', ':BufferClose<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-o>', ':Telescope find_files<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-f>', ':Telescope live_grep<CR>', { noremap = true })
+
+vim.api.nvim_create_autocmd('BufWritePre', { pattern = '*.rs', command = 'RustFmt' } )
+vim.api.nvim_create_autocmd('BufReadPre,FileReadPre', { pattern = '*.md', command = 'set wrap linebreak' } )
+
+vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<CR>"', { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-j>', 'coc#float#has_scroll() ? "<C-R>=coc#float#scroll(1)<CR>" : "<Right>"', { nowait = true, expr = true })
+vim.api.nvim_set_keymap('i', '<C-k>', 'coc#float#has_scroll() ? "<C-R>=coc#float#scroll(0)<CR>" : "<Left>"', { nowait = true, expr = true })
+
+vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true })
 
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,4 +1,4 @@
-return require('packer').startup(function()
+require('packer').startup(function()
   -- packer itself 
   use 'wbthomason/packer.nvim'
 
@@ -22,5 +22,30 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = { 'nvim-lua/plenary.nvim' }
   }
+
+  -- Rusty goods
+  use 'rust-lang/rust.vim'
+
+  -- Visual feedback for git status
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  -- File tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  -- Comment stuff out
+  use "terrortylor/nvim-comment"
 end)
+
+-- Initialize plugins
+
+require("nvim-tree").setup()                                                                                            
+require('nvim_comment').setup({comment_empty = false})
 
